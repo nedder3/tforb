d06802c1c105e585f880955355b210fe751cda5c
nedder3 <arij.dev@gmail.com>
2023-06-01 17:27:55 -0300

    ADD transaction card w/springboot notations

---
+++ backend/src/main/java/com/api/backend/models/Card.java
@@ -0,0 +1,22 @@
+package com.api.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="cards")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cardNumber;
+    private String placeHolder;
+    private String expiryDate;
+}
