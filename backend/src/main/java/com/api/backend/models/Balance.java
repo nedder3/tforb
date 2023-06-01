89496e05451d6d52458764c188f908fd849e0e56
nedder3 <arij.dev@gmail.com>
2023-06-01 16:57:04 -0300

    ADD balance model w/springboot notations

--- backend/src/main/java/com/api/backend/models/Balance.java
+++ backend/src/main/java/com/api/backend/models/Balance.java
@@ -1,2 +1,24 @@
-package com.api.backend.models;public class Balance {
+package com.api.backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Balance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double amount;
+
+    private String currency;
+
+
 }
