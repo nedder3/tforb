652396fbe778a254ad75bb771f7bf3ed37478d15
nedder3 <arij.dev@gmail.com>
2023-06-02 04:26:49 -0300

    ADD dto layer implementation and userDTO

--- backend/src/main/java/com/api/backend/controllers/UserRestController.java
+++ backend/src/main/java/com/api/backend/controllers/UserRestController.java
@@ -3,6 +3,8 @@ package com.api.backend.controllers;
 import com.api.backend.models.User;
 import com.api.backend.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,43 +21,46 @@ public class UserRestController {
     public UserRestController(UserServiceImpl userServiceImpl) {
         this.userServiceImpl = userServiceImpl;
     }
-
-    @PostMapping(value="create_user",headers = "Accept=application/json")
-    public void createUser(@RequestBody User user){
+    @PostMapping(value = "create_user", headers = "Accept=application/json")
+    public ResponseEntity<Void> createUser(@RequestBody User user) {
         userServiceImpl.createUser(user);
-    }
-    @GetMapping(value="get_all_users",headers = "Accept=application/json")
-    public List<User> getAllUsers(){
-     return userServiceImpl.getAllUsers();
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping(value="get_user/{id}",headers = "Accept=application/json")
-    public Optional<User> findById(@PathVariable Long id){
-        return userServiceImpl.getUserById(id);
+    @GetMapping(value = "get_all_users", headers = "Accept=application/json")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userServiceImpl.getAllUsers();
+        return ResponseEntity.ok(users);
     }
 
-    @PutMapping(value="update_user",headers = "Accept=application/json")
-    public void updateUser(@RequestBody User user){
+    @GetMapping(value = "get_user/{id}", headers = "Accept=application/json")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        Optional<User> user = userServiceImpl.getUserById(id);
+        return user.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(value = "update_user", headers = "Accept=application/json")
+    public ResponseEntity<Void> updateUser(@RequestBody User user) {
         userServiceImpl.updateUser(user);
+        return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping(value="delete_user/{id}",headers = "Accept=application/json")
-    public void deleteUser(@PathVariable Long id ){
-         userServiceImpl.deleteUser(id);
+    @DeleteMapping(value = "delete_user/{id}", headers = "Accept=application/json")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userServiceImpl.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 
-    //getAllUserByIdentificationType
-    @GetMapping(value="get_user/",headers = "Accept=application/json")
-    public List<User> findByIdentificationType(@RequestParam("identificationType") String identificationType){
-        return userServiceImpl.getAllUserByIdentificationType(identificationType);
+    @GetMapping(value = "get_user/", headers = "Accept=application/json", params = "identificationType")
+    public ResponseEntity<List<User>> findByIdentificationType(@RequestParam("identificationType") String identificationType) {
+        List<User> users = userServiceImpl.getAllUserByIdentificationType(identificationType);
+        return ResponseEntity.ok(users);
     }
 
-
-
-    //getUserByUserIdentificationNumber
-    @GetMapping(value="get_user",headers = "Accept=application/json")
-    public List<User> findByUserIdentificationNumber(@RequestParam ("userIdentificationNumber") Integer userIdentificationNumber){
-        return userServiceImpl.getUserByUserIdentificationNumber(userIdentificationNumber);
+    @GetMapping(value = "get_user", headers = "Accept=application/json", params = "userIdentificationNumber")
+    public ResponseEntity<List<User>> findByUserIdentificationNumber(@RequestParam("userIdentificationNumber") Integer userIdentificationNumber) {
+        List<User> users = userServiceImpl.getUserByUserIdentificationNumber(userIdentificationNumber);
+        return ResponseEntity.ok(users);
     }
 
 }
---
+++ backend/src/main/java/com/api/backend/dtos/UserDTO.java
@@ -0,0 +1,10 @@
+package com.api.backend.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Integer userIdentificationNumber;
+}
